ccda474fcfee3dad21923d0b11bb476bba6c8898
Trung Luu <luuductrung1234@gmail.com>
2024-12-25 16:40:36 +0700

    update

--- cli/020-user-pool-function.sh
+++ cli/020-user-pool-function.sh
@@ -6,5 +6,6 @@ REGION=${3:-ap-northeast-1}
 
 aws cognito-idp update-user-pool \
   --user-pool-id $USER_POO_ID \
+  --auto-verified-attributes "email" \
   --lambda-config PostConfirmation=$LAMBDA_ARN \
   --region $REGION
